e263d1ea7f0c66bfe240258bffa0d664de0720bf
jcrea4 <jcrea4@gmail.com>
2017-08-20 12:11:10 -0700

    Insert try-except for invalid inputs with challenge 4.

--- ch7/challenge4.py
+++ ch7/challenge4.py
@@ -6,7 +6,11 @@ while True:
     input_val = input("Guess a number (type q to quit): ")
     if input_val == 'q':
         break
-    guess = int(input_val)
+    try:
+        guess = int(input_val)
+    except ValueError:
+        print('Please type a number or q to quit.')
+        continue
     if guess in numbers:
         print('Correct! ' + input_val + ' is in the list of numbers.')
     else:
